79966b3647466c9216848e683520df70a50428da
shohei <shohei.miyagi@devenues.com>
2022-12-28 15:21:20 +0900

    js

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+users = [
+  ["たけし", "hoge@hoge", "男", 20],
+  ["さおり", "hoge@hoge", "女", 23],
+  ["たろう", "hoge@hoge", "男", 22]
+]
+
+users.each do |name, email, sex, age|
+  User.create(name: name, email: email, sex: sex, age: age)
+end
